5b6984a21c5074a0712b54cad95cc56273897086
n0aht <n.telerman@gmail.com>
2024-09-05 13:17:36 -0400

    separating plotting utilities

--- analysis2.py
+++ analysis2.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import patch_utils as patch
+import plot_utils as p_plot
 import seaborn as sms
 
 datafile_voltage = r'C:\Users\jl5675\Desktop\Noah\data\patch2\rec1_cell1-V.csv'
@@ -13,30 +14,31 @@ datafile_current = r'C:\Users\jl5675\Desktop\Noah\data\patch2\rec1_cell1-I.csv'
 
 data_V, metadata_V = patch.extract_data_and_metadata(datafile_voltage)
 data_I, metadata_I = patch.extract_data_and_metadata(datafile_current)
-
+print(data_V.iloc[0][0])
 # %%
+p_plot.multiplot(data_V, metadata_V, 'DIV 16')
 
-num_points = int(metadata_V.num_points.value)
-resolution = int(metadata_V.resolution.value)
-x_axis = np.linspace(0, (num_points / resolution), num_points)
-
-# multi plot
-multiplot = plt.figure(figsize=(10, 6))
-for i in range(len(data_V)):
-    plot_data = data_V.iloc[i].tolist()
-    plt.plot(x_axis, plot_data, linestyle='-')
-
-# Add titles and labels
-# for these cells, day 0 was August 12th
-plt.title('Current Clamp Recording - DIV 16')
-plt.xlabel('Time (ms)')
-plt.ylabel('Voltage (mV)')
-
-# Show grid
-plt.grid(True)
-
-# Display the plot
-plt.show()
+# num_points = int(metadata_V.num_points.value)
+# resolution = int(metadata_V.resolution.value)
+# x_axis = np.linspace(0, (num_points / resolution), num_points)
+#
+# # multi plot
+# multiplot = plt.figure(figsize=(10, 6))
+# for i in range(len(data_V)):
+#     plot_data = data_V.iloc[i].tolist()
+#     plt.plot(x_axis, plot_data, linestyle='-')
+#
+# # Add titles and labels
+# # for these cells, day 0 was August 12th
+# plt.title('Current Clamp Recording - DIV 16')
+# plt.xlabel('Time (ms)')
+# plt.ylabel('Voltage (mV)')
+#
+# # Show grid
+# plt.grid(True)
+#
+# # Display the plot
+# plt.show()
 
 # %%
 
