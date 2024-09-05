1752d947df6d07c4f71a80e77a3b2b8c66d727a6
n0aht <n.telerman@gmail.com>
2024-09-05 13:15:41 -0400

    separating plotting utilities

---
+++ plot_utils.py
@@ -0,0 +1,74 @@
+# plotting utilities for patch clamp recordings
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def multiplot(data: pd.DataFrame, metadata: pd.DataFrame, title: str) -> None:
+    '''
+    This function plots patch data recorded via jesus' labview program that has been processed by the
+    extract_data_and_metadata() function in patch utils
+
+    Plots each trial in one plot on top of one another
+
+    :param data: Voltage dataframe of recorded data
+    :param metadata: dataframe of metadata from the recording
+    :return: void, no value is returned
+    '''
+
+    num_points = int(metadata.num_points.value)
+    resolution = int(metadata.resolution.value)
+    x_axis = np.linspace(0, (num_points / resolution), num_points)
+
+    # multi plot
+    fig = plt.figure(figsize=(10, 6))
+    for i in range(len(data)):
+        plot_data = data.iloc[i].tolist()
+        plt.plot(x_axis, plot_data, linestyle='-')
+
+    # Add titles and labels
+    # for these cells, day 0 was August 12th
+    plt.title(title)
+    plt.xlabel('Time (ms)')
+    plt.ylabel('Voltage (mV)')
+
+    # Show grid
+    plt.grid(True)
+
+    # Display the plot
+    plt.show()
+
+def time_plot(data_voltage: pd.DataFrame, data_current: pd.DataFrame, metadata: pd.DataFrame, title: str = None) -> None:
+    '''
+    plot all data and voltage recordings over time in a recording
+
+    :param data_voltage:
+    :param data_current:
+    :param metadata:
+    :return:
+    '''
+
+    # plot with current
+
+    # Concatenate all lists into one long list
+    data_long_V = data_voltage.values.flatten()
+    data_long_I = data_current.values.flatten()
+    resolution = int(metadata.resolution.value)
+    x_axis_long = np.linspace(0, len(data_long_V) / resolution, len(data_long_V))
+
+    fig_long, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 6), sharex=True)
+
+    # First subplot
+    ax1.plot(x_axis_long, data_long_V, color='b', linestyle='-')
+    ax1.set_title(title)
+    ax1.set_ylabel('Voltage (mV)')
+    ax1.grid(True)
+
+    # Second subplot
+    ax2.plot(x_axis_long, data_long_I, color='r', linestyle='--')
+    ax2.set_title(title)
+    ax2.set_xlabel('Time (ms)')
+    ax2.set_ylabel('Current (pA)')
+    ax2.grid(True)
+
+    plt.show()
